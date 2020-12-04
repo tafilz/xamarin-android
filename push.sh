1b221d4542b0ac03e3ffdbd2201960ce2cf312d3
Tafil Avdyli <t.avdyli@hotmail.de>
2020-12-04 23:38:19 +0100

    Add api 30 to push script

--- push.sh
+++ push.sh
@@ -10,9 +10,11 @@ docker push tafilz/xamarin-android:26-ndk-latest
 docker push tafilz/xamarin-android:27-ndk-latest
 docker push tafilz/xamarin-android:28-ndk-latest
 docker push tafilz/xamarin-android:29-ndk-latest
+docker push tafilz/xamarin-android:30-ndk-latest
 
 # API Levels
 docker push tafilz/xamarin-android:26-latest
 docker push tafilz/xamarin-android:27-latest
 docker push tafilz/xamarin-android:28-latest
 docker push tafilz/xamarin-android:29-latest
+docker push tafilz/xamarin-android:30-latest
