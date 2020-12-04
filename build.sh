316cdd3f95241bc28a03a36ca521431ba85ef402
Tafil Avdyli <t.avdyli@hotmail.de>
2020-12-04 22:28:09 +0100

    Add api level 30
    * do not push platform and ndk layer
    * TODO naming scheme (keep old images)

--- build.sh
+++ build.sh
@@ -6,13 +6,15 @@ docker build -t tafilz/xamarin-android:platform-only ./platform
 docker build -t tafilz/xamarin-android:ndk-only ./ndk
 
 # API Levels + NDK
-docker build -t tafilz/xamarin-android:26-ndk ./26/ndk
-docker build -t tafilz/xamarin-android:27-ndk ./27/ndk
-docker build -t tafilz/xamarin-android:28-ndk ./28/ndk
-docker build -t tafilz/xamarin-android:29-ndk ./29/ndk
+docker build -t tafilz/xamarin-android:26-ndk-latest ./26/ndk
+docker build -t tafilz/xamarin-android:27-ndk-latest ./27/ndk
+docker build -t tafilz/xamarin-android:28-ndk-latest ./28/ndk
+docker build -t tafilz/xamarin-android:29-ndk-latest ./29/ndk
+docker build -t tafilz/xamarin-android:30-ndk-latest ./30/ndk
 
 # API Levels
-docker build -t tafilz/xamarin-android:26 ./26
-docker build -t tafilz/xamarin-android:27 ./27
-docker build -t tafilz/xamarin-android:28 ./28
-docker build -t tafilz/xamarin-android:29 ./29
+docker build -t tafilz/xamarin-android:26-latest ./26
+docker build -t tafilz/xamarin-android:27-latest ./27
+docker build -t tafilz/xamarin-android:28-latest ./28
+docker build -t tafilz/xamarin-android:29-latest ./29
+docker build -t tafilz/xamarin-android:30-latest ./30
--- push.sh
+++ push.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Platform Layer
-docker push tafilz/xamarin-android:platform-only
+#docker push tafilz/xamarin-android:platform-only
 # NDK Layer
-docker push tafilz/xamarin-android:ndk-only
+#docker push tafilz/xamarin-android:ndk-only
 
 # API Levels + NDK
-docker push tafilz/xamarin-android:26-ndk
-docker push tafilz/xamarin-android:27-ndk
-docker push tafilz/xamarin-android:28-ndk
-docker push tafilz/xamarin-android:29-ndk
+docker push tafilz/xamarin-android:26-ndk-latest
+docker push tafilz/xamarin-android:27-ndk-latest
+docker push tafilz/xamarin-android:28-ndk-latest
+docker push tafilz/xamarin-android:29-ndk-latest
 
 # API Levels
-docker push tafilz/xamarin-android:26
-docker push tafilz/xamarin-android:27
-docker push tafilz/xamarin-android:28
-docker push tafilz/xamarin-android:29
+docker push tafilz/xamarin-android:26-latest
+docker push tafilz/xamarin-android:27-latest
+docker push tafilz/xamarin-android:28-latest
+docker push tafilz/xamarin-android:29-latest
